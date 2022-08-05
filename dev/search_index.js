a52b79008866b199c6e314e0329784c6d6744f43
Documenter.jl <documenter@juliadocs.github.io>
2022-08-05 11:32:01 +0000

    build based on 13823f7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TuePlots","category":"page"},{"location":"#TuePlots","page":"Home","title":"TuePlots","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TuePlots.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TuePlots]","category":"page"}]
+}
