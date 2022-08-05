36b76c75a3c6cd66dd88de1097f9763775192c65
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-08-05 14:54:46 +0200

    Add JuliaFormatter.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,25 +1,25 @@
 using TuePlots
 using Documenter
 
-DocMeta.setdocmeta!(TuePlots, :DocTestSetup, :(using TuePlots); recursive=true)
+DocMeta.setdocmeta!(TuePlots, :DocTestSetup, :(using TuePlots); recursive = true)
 
 makedocs(;
-    modules=[TuePlots],
-    authors="Nathanael Bosch <nathanael.bosch@uni-tuebingen.de> and contributors",
-    repo="https://github.com/nathanaelbosch/TuePlots.jl/blob/{commit}{path}#{line}",
-    sitename="TuePlots.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://nathanaelbosch.github.io/TuePlots.jl",
-        edit_link="main",
-        assets=String[],
+    modules = [TuePlots],
+    authors = "Nathanael Bosch <nathanael.bosch@uni-tuebingen.de> and contributors",
+    repo = "https://github.com/nathanaelbosch/TuePlots.jl/blob/{commit}{path}#{line}",
+    sitename = "TuePlots.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://nathanaelbosch.github.io/TuePlots.jl",
+        edit_link = "main",
+        assets = String[],
     ),
-    pages=[
+    pages = [
         "Home" => "index.md",
     ],
 )
 
 deploydocs(;
-    repo="github.com/nathanaelbosch/TuePlots.jl",
-    devbranch="main",
+    repo = "github.com/nathanaelbosch/TuePlots.jl",
+    devbranch = "main",
 )
