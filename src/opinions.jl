1dde6c74ffffdf7837f90ef2848d607e1d3c56b8
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-08-14 17:18:43 +0200

    Refactor TuePlots.jl: Group settings by conf, not setting type

--- src/opinions.jl
+++ src/opinions.jl
@@ -1,8 +1,7 @@
-
 """
 Theme with thinner lines everywhere to suit the typically smaller figure sizes in papers.
 """
-MakieThinTheme() = MakieCore.Theme(
+const MakieThinTheme = MakieCore.Theme(
     figure_padding = 5,
     Axis = (
         spinewidth = 0.5,
---
+++ src/to_plotsjl.jl
@@ -0,0 +1,40 @@
+function get_plotsjl_theme_kwargs(
+    setting::TuePlotsSetting;
+    font = false, fontsize = true, figsize = :full, thickness_scaling = true
+)
+    kwargs = (;)
+
+    if font
+        @error "Fonts are not yet supported for Plots.jl"
+        # kwargs = merge(kwargs, (; xlabelfont = "Times New Roman", xlabelfontfamily="serif"))
+    end
+
+    if fontsize
+        main, small = base_fontsize_to_sizes(setting.base_fontsize)
+        kwargs = merge(
+            kwargs,
+            (
+                titlefontsize = main,
+                legend_font_pointsize = main,
+                legend_title_font_pointsize = main,
+                tickfontsize = small,
+                labelfontsize = main,
+            ),
+        )
+    end
+
+    if figsize == :full
+        resolution = width_to_resolution(setting.width_half)
+        kwargs = merge(kwargs, (; size = resolution))
+    elseif figsize == :half && setting.width_half isa Number
+        resolution = width_to_resolution(setting.width_half)
+        kwargs = merge(kwargs, (; size = resolution))
+    end
+
+    if thickness_scaling
+        # somehow necessary to get the same font sizes as with Makie.jl
+        kwargs = merge(kwargs, (; thickness_scaling = 0.7))
+    end
+
+    return kwargs
+end
