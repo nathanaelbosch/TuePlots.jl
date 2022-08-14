cf902038afd11ef307ace017d6551b29d8d5b91c
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-08-14 18:17:04 +0200

    Add some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,22 @@
 using TuePlots
-using Test
+using Test, SafeTestsets, Aqua
+import MakieCore, Plots
 
 @testset "TuePlots.jl" begin
-    # Write your tests here.
+    setting_keys = TuePlots.get_available_settings()
+
+    @testset "Makie themes" begin
+        @testset "$k" for k in setting_keys
+            s = TuePlots.SETTINGS[k]
+            @test MakieCore.Theme(s) isa MakieCore.Theme
+        end
+    end
+
+    @testset "Plots.jl themes" begin
+        @testset "$k" for k in setting_keys
+            s = TuePlots.SETTINGS[k]
+            @test Plots.theme(:default; TuePlots.get_plotsjl_theme_kwargs(s)...) |>
+                  isnothing
+        end
+    end
 end
